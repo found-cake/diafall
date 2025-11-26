4c3dff771b45b02766d305debf69a6f57cefe03f
found-cake <me@foundcake.kr>
2025-11-27 00:36:02 +0900

    아이테 공유 방지

--- src/main/kotlin/EventListener.kt
+++ src/main/kotlin/EventListener.kt
@@ -5,6 +5,7 @@ import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntityDamageEvent
+import org.bukkit.event.player.PlayerDropItemEvent
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerQuitEvent
 
@@ -25,5 +26,11 @@ class EventListener: Listener {
         if (event.entity !is Player )return
         if (event.cause != EntityDamageEvent.DamageCause.FALL) return
         DiafallManager.diafallCheck(event.entity as Player)
+    }
+
+    @EventHandler
+    fun onItem(event: PlayerDropItemEvent) {
+        event.itemDrop.owner = event.player.uniqueId
+        event.itemDrop.setWillAge(true)
     }
 }
